01fbb30abf02085ce7d79d7b5d918a14bccff556
Kyle Coberly <kyle.coberly@gmail.com>
2021-06-15 21:42:35 -0600

    Removing solution

--- index.js
+++
@@ -1,7 +0,0 @@
-function displayEmail() {
-  const email = document.querySelector("#email-input").value
-  document.querySelector("#email-output").textContent = email
-}
-
-const button = document.querySelector("#print")
-button.onclick = displayEmail
