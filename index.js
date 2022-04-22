cc1c58303a2a3017322d0cddf6c222ba3ee56b4e
tunnelrat16 <tjandmatt@hotmail.com>
2022-04-22 10:29:04 -0700

    add .textContent

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const output = document.querySelector("#email-output")
 printButton.addEventListener("click", event => {
     event.preventDefault();
     const input = document.querySelector("#email-input").value
-    output.innerHTML = input;
+    output.textContent = input;
 })
